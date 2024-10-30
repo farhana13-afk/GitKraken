da64f3c8e8a28bed2361891907c965cdbc209a0f
farhana13-afk <farhanaashiq01@gmail.com>
2024-10-30 13:42:40 -0400

    Commit 2
    find the sum of 1 - 1000000 in each thread

--- GithubKraken.java
+++ GithubKraken.java
@@ -11,6 +11,11 @@ public class GithubKraken implements Runnable
 
     @Override
     public void run() {
-
+        int sum =0;
+        for(int i=1; i<=1000000; i++)
+        {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
